96444b5e8a0df2533dbda69621aefa51971bc2a5
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-20 15:12:16 +0200

    :construction: add functions with typed parreturning types

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,28 @@
-function startGame() {
-    let playerName: string = 'Geisy';
+function startGame(e:Event) {
+    e.preventDefault()
+    let playerName: string | undefined = getInputValue('playerName');
     logPlayer(playerName);
+
+    postScore(100, playerName);
 }
 
-function logPlayer (name) {
+function logPlayer (name: string = 'MultiMath Player'): void {
     console.log(`player name is: ${name}`)
 }
 
-document.querySelector('#startGame')!.addEventListener('click', startGame)
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.querySelector(`#${elementId}`)
+
+    if (inputElement.value === '') {
+        return undefined
+    } else {
+        return inputElement.value
+    }
+}
+
+function postScore(score: number, playerName?: string) : void {
+    const scoreElement: HTMLElement | null = document.querySelector('#postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+document.querySelector('#startGame')!.addEventListener('submit', startGame)
